aaeb7ed37c02c3c2a9e6870b8c2c49bc561657de
mr-dark-codex <darkcodm123@gmail.com>
2025-08-19 17:41:33 +0530

    update db schema

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    status: { type: String, enum: ["todo", "inprogress", "done"], default: "todo" },
+    status: {
+      type: String,
+      enum: ["todo", "inprogress", "done"],
+      default: "todo",
+    },
   },
   { timestamps: true }
 );
